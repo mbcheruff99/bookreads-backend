4a2987c1915c1ef6e249965eb41f895328f9710b
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-11-27 22:32:31 -0500

    populated the seed with faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 10.times do
-  actor = Actor.create(
-    
+  actor = Author.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    bio: Faker::Lorem.paragraphs
   )
 end
