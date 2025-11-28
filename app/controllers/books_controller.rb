41a716f7fefd26f39de93afe72cb766af1c0ffd1
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-11-27 23:31:01 -0500

    added index, show, and create actions to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,26 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    render :index
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    render :show
+  end
+
+  def create
+    @book = Book.new(
+      title: params[:title],
+      author_id: params[:author_id]
+    )
+
+    if @book.save
+      render :show
+    else
+      render json: { errors: @book.errors }, status: :unprocessable_entity
+    end
+  end
+
 end
