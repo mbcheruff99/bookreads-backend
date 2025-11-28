69451d468017603a3c3bcb306516aae96cb66f32
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-11-27 23:17:47 -0500

    added create action to authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -24,5 +24,19 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def update
+    @author = Author.find(params[:id])
+    @author.update(
+      first_name: params[:first_name] || @author.first_name,
+      last_name: params[:last_name] || @author.last_name,
+      bio: params[:bio] || @author.bio
+    )
+
+    if @author.valid?
+      render :show
+    else
+      render json: { errors: @author.errors }, status: :unprocessable_entity
+    end
+  end
 
 end
