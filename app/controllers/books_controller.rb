a68db91ea088fbc7986a4e16f4a7367446fca47a
bshin27 <125755440+bshin27@users.noreply.github.com>
2025-11-27 23:36:49 -0500

    added update and destroy actions to books controller. All CRUD actions are completed for books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,5 +22,25 @@ class BooksController < ApplicationController
       render json: { errors: @book.errors }, status: :unprocessable_entity
     end
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(
+      title: params[:title] || @book.title,
+      author_id: params[:author_id] || @book.author_id
+    )
+    
+    if @book.valid?
+      render :show
+    else
+      render json: { errors: @book.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    render json: { message: "Book deleted" }
+  end
 
 end
